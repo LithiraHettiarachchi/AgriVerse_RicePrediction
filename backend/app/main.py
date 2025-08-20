9eebda4ed25bd1847197a433704853ff760f1ea5
sanjula2301 <sanjula2301@gmail.com>
2025-08-20 12:06:08 +0530

    Enable the cross-origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI, APIRouter
 from app.routes import prediction
 from app.routes import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+# allow specific origins
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(prediction.router, tags=["Paddy Prediction"])
 app.include_router(auth.router, tags=["Auth"])
